e124cfe6d41698c54a19103187d7b092e094c8a2
rl627 <rl627@drexel.edu>
2021-11-11 13:38:03 -0500

    Bivariate Beeswarm composed

--- App/R/Modules/BivariateRelationship/BivariateRelationship.R
+++ App/R/Modules/BivariateRelationship/BivariateRelationship.R
@@ -9,7 +9,7 @@ BivariateRelationship_UI <- function(id) {
     div( class = "bivarPanel", 
          tabsetPanel(
            tabPanel("Map", uiOutput(ns("sync_map")) ),
-           tabPanel("Beeswarm","BEE")
+           tabPanel("Beeswarm", UnivariateBeeswarm_UI(ns('distribution')))
          ))
   )
 }
@@ -17,11 +17,30 @@ BivariateRelationship_UI <- function(id) {
 BivariateRelationship_Server <- function(id, data, options,page){
   moduleServer(id,function(input, output, session) {
     
+    ### Data
     dataFiltered <- InputForm_Server('input',data,options,list('metric'=T,'by'=F,'age'=T))
+    bivarData = reactive({
+      print("***")
+      df_bivar = dataFiltered() %>%
+        select(salid1, by = cat) %>%
+        left_join(data %>% filter(metric == "Mean Temperature") %>% filter(age == "Crude"))
+
+      print(df_bivar)
+
+      df_bivar
+    })
+    ### Distribution
+    UnivariateBeeswarm_Server('distribution',bivarData)
     
+    ### Sync Map
     output$sync_map = renderUI({output$sync_map = renderUI({
-      ## Map 1
-      data1 = data %>% filter(metric == "RR at P99") %>% filter(age == "Crude")
+      print("**sync_map**")
+      req(dataFiltered())
+      print("*")
+      validate(need(nrow( dataFiltered() >1),"Need Data"))
+      data1 = dataFiltered() 
+    
+      print(data1)
       pal <-  colorNumeric("plasma", data1$value, reverse = F)
       leaflet1 = leaflet(data = data1) %>%
         addProviderTiles("Esri.WorldGrayCanvas") %>%
@@ -34,9 +53,6 @@ BivariateRelationship_Server <- function(id, data, options,page){
                   title = str_wrap_leaflet_legend_title(unique(data1$metric)),
                   pal = pal, values = ~value)
       
-      
-      
-      ## Map 2
       data2 =  data %>% filter(metric == "Mean Temperature") %>% filter(age == "Crude")
       pal <-  colorNumeric("plasma", data2$value, reverse = F)
       leaflet2 = leaflet(data = data2) %>%
@@ -50,7 +66,6 @@ BivariateRelationship_Server <- function(id, data, options,page){
                   title = str_wrap_leaflet_legend_title(unique(data2$metric)),
                   pal = pal, values = ~value)
       
-      ## Sync Map
       leafsync::sync(leaflet1,leaflet2, sync.cursor = F)
     })})
   })
