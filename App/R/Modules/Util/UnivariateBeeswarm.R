b67dfec3324f5f08caba78de7ac72bddf606c50d
rl627 <rl627@drexel.edu>
2021-11-16 18:07:31 -0500

    Generalized UnivariateBeeswarm module to accept a stratified by argument

--- App/R/Modules/Util/UnivariateBeeswarm.R
+++ App/R/Modules/Util/UnivariateBeeswarm.R
@@ -5,36 +5,40 @@ UnivariateBeeswarm_UI <- function(id) {
   )
 }
 
-UnivariateBeeswarm_Server <- function(id,dataFiltered ){
+UnivariateBeeswarm_Server <- function(id,dataFiltered,by ){
   moduleServer(id,function(input, output, session) {
     
     
     output$swarm <- renderPlotly({
-      data = dataFiltered()
-      if (is.null(data)) return()
-      validate( need(nrow(data)>1, 'No Data Available, please select another attribute combination.'))
+      dataFiltered = dataFiltered() %>% 
+        mutate_(by={{by}}) 
+      
+      
+      if (is.null(dataFiltered)) return()
+      validate( need(nrow(dataFiltered)>1, 'No Data Available, please select another attribute combination.'))
       
 
       ## Arrange by stratified varianble average
-      xwalk_factor_order = data %>% 
+      xwalk_factor_order = dataFiltered %>% 
         group_by(by) %>% 
         summarize(value = mean(value)) %>% 
         ungroup() %>% 
         arrange((value))
       
-      dfTmpOrdered = data %>% mutate(by = factor(by, levels = xwalk_factor_order$by))
+      dfTmpOrdered = dataFiltered %>% mutate(by = factor(by, levels = xwalk_factor_order$by))
       
       ## Var Label
       var_short_tmp = unique(dfTmpOrdered$metric)
       
-      ## 
+      ## Plot
       gg = dfTmpOrdered %>% 
         ggplot(mapping=aes(value,by, text = tooltip__beeswarmPlotly)) + 
         geom_quasirandom(aes(value,by,color=by),
                          groupOnX=FALSE)+
         theme_minimal()+
         labs(y = "",
-             x= var_short_tmp)
+             x= var_short_tmp)+ 
+        scale_color_discrete(name = str_to_title(by))
       
       
       ggplotly(gg, tooltip = 'text' )
